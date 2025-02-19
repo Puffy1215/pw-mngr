86845911f4db867f2f6f8777c895f50239f20753
Andy Xie <andy.xie.opensource@gmail.com>
2025-02-19 23:45:07 +0800

    Initial implementation of validation functions

--- client.py
+++ client.py
@@ -1,18 +1,33 @@
 import socket
+import hashlib
 import pickle
 
-def set_password():
-    pass
+def login(sock: socket.socket, master_pass: str) -> bool:
+    master_pass_dict = pickle.dumps({"master_pw": master_pass})
+    sock.send(master_pass_dict)
 
-def retrieve_password():
+    sock.settimeout(15)
+    success_or_fail, _ = sock.accept()
+    
+    return False
+
+
+def set_password(pw_dict: dict, master_pass: str):
+    sock = socket.socket()
+    sock.connect(("127.0.0.1", 1234))
+
+    if login(sock, master_pass):
+        data = pickle.dumps(pw_dict)    
+        
+        sock.send(data)
+        sock.close()
+
+def retrieve_password(pw_key: dict, master_pass: str):
     pass
 
 def main():
-    data = pickle.dumps({"test": "test1"})
-    sock = socket.socket()
-    sock.connect(("127.0.0.1", 1234))
-    sock.send(data)
-    sock.close()
+    master_pass = input("Enter master password: ")
+    set_password({"test": "test1"}, master_pass)
 
 if __name__ == '__main__':
     main()
--- manager.py
+++ manager.py
@@ -1,21 +1,35 @@
 import socket
+import hashlib
+import os
+import sys
 import pickle
 
 HOST = 'localhost'
 PORT = 1234
 
-sock = socket.create_server((HOST, PORT))
-
 passwords = {}
+
+def _generate_master_pass(password: str):
+    salt = os.urandom(50)
+    master_pw = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 862780)
+    return master_pw.hex(), salt
+
+def validate_login(master_pass_obj: dict, salt: bytes, master_pass_hex: str) -> bool:
+    password = master_pass_obj.get("master_pw", "")
+    if password == "":
+        return ""
+    password_hash = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 862780)
+    return password_hash.hex() == master_pass_hex
+
 def main():
+    master_pw_hex, salt = _generate_master_pass(sys.argv[1])
+    sock = socket.create_server((HOST, PORT))
     sock.listen()
     while True: 
         conn, address = sock.accept()
         obj = pickle.loads(conn.recv(1024))
-        print(f'Connected with {address[0]}, {obj}')
-        
-        passwords.update(obj)
-        print(passwords)
+        print(validate_login(obj, salt, master_pw_hex))
+
 
 
 if __name__ == '__main__':
