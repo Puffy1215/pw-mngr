c1b195d2642a103b3e27a2fd859eee12d04b5607
Andy Xie <andy.xie.opensource@gmail.com>
2025-02-21 21:56:47 +0800

    Added fernet crypt and used microsoft black formatter

--- client.py
+++ client.py
@@ -1,43 +1,57 @@
 import socket
 import hashlib
 import pickle
+import base64
 from getpass import getpass
+from typing import Tuple
+from cryptography.fernet import Fernet
 
-def login(sock: socket.socket, master_pass: str) -> bool:
-    salt = pickle.loads(sock.recv(1024))['salt']
-    hex = hashlib.pbkdf2_hmac('sha256', master_pass.encode(), salt, 862780).hex()
+
+def login(sock: socket.socket, master_pass: str) -> Tuple[bool, bytes]:
+    salt = pickle.loads(sock.recv(1024))["salt"]
+    master_pass_bytes = hashlib.pbkdf2_hmac(
+        "sha256", master_pass.encode(), salt, 862780
+    )
     master_pass = 0
-    master_pass_dict = pickle.dumps({"master_pw": hex})
+    master_pass_dict = pickle.dumps({"master_pw": master_pass_bytes.hex()})
 
     sock.send(master_pass_dict)
 
     sock.settimeout(15)
     success_or_fail = sock.recv(1024)
-    return bool.from_bytes(success_or_fail), hex, salt
-        
+    return bool.from_bytes(success_or_fail), master_pass_bytes
 
-def set_password(sock: socket.socket, set_pass: str, master_pass_hex: str, username: str):
-    new_hex = (hashlib.pbkdf2_hmac('sha256', set_pass.encode(), username.encode(), 862780) + bytes.fromhex(master_pass_hex)).hex()
-    print(new_hex)
-    data = pickle.dumps({username: new_hex})
+
+def set_password(sock: socket.socket, set_pass: str, username: str, fern: Fernet):
+    token = fern.encrypt(set_pass.encode())
+    print(token)
+
+    data = pickle.dumps({username: token})
     print("sending password")
     sock.send(data)
 
-def retrieve_password(pw_key: dict, master_pass: str):
+
+def retrieve_password(username: str, master_pass_hex: str):
     pass
+
 
 def main():
     sock = socket.socket()
     sock.connect(("127.0.0.1", 1234))
     master_pass = getpass("Enter master password: ")
 
-    is_login_valid, hex, salt = login(sock, master_pass)
+    is_login_valid, master_pass_bytes = login(sock, master_pass)
     master_pass = 0
     if is_login_valid:
         print("login complete")
+        fern = Fernet(base64.urlsafe_b64encode(master_pass_bytes))
         username = input("Enter a username for the password: ")
         set_pass = getpass("Enter a password to store: ")
-        set_password(sock, set_pass, hex, username)
+        set_password(sock, set_pass, username, fern)
+    else:
+        print("login failed")
+        sock.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- manager.py
+++ manager.py
@@ -3,21 +3,25 @@ import hashlib
 import os
 import sys
 import pickle
+from cryptography.fernet import Fernet
 
-HOST = 'localhost'
+HOST = "localhost"
 PORT = 1234
 
 passwords = {}
 
+
 def _generate_master_pass(password: str):
     salt = os.urandom(50)
-    master_pw = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 862780)
+    master_pw = hashlib.pbkdf2_hmac("sha256", password.encode(), salt, 862780)
     return master_pw.hex(), salt
+
 
 def validate_login(conn: socket.socket, salt: bytes, master_pass_hex: str) -> bool:
     conn.send(pickle.dumps({"salt": salt}))
-    hex = pickle.loads(conn.recv(1024))['master_pw']
+    hex = pickle.loads(conn.recv(1024))["master_pw"]
     return hex == master_pass_hex
+
 
 def main():
     master_pw_hex, salt = _generate_master_pass(sys.argv[1])
@@ -25,17 +29,17 @@ def main():
 
     sock = socket.create_server((HOST, PORT))
     sock.listen()
-    while True: 
+    while True:
         conn, _ = sock.accept()
         is_login_valid = validate_login(conn, salt, master_pw_hex)
+        conn.send(is_login_valid.to_bytes())
         if is_login_valid:
-            conn.send(is_login_valid.to_bytes())
             print("success")
-            new_pw_bytes = conn.recv(4096)
+            new_pw_bytes = conn.recv(1024)
             print(new_pw_bytes)
             new_pw = pickle.loads(new_pw_bytes)
             print(new_pw)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
